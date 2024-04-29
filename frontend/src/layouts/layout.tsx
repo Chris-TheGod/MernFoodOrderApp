63dc4374a4146e64b25048bc742f00aba26a65e8
Kristjan Lääne <xonproject@gmail.com>
2024-04-29 19:06:17 +0300

    Layout start

---
+++ frontend/src/layouts/layout.tsx
@@ -0,0 +1,9 @@
+type Props = {
+  children: React.ReactNode;
+};
+
+const layout = ({ children }: Props) => {
+  return <div className='felx felx-col min-h-screen'></div>;
+};
+
+export default layout;
