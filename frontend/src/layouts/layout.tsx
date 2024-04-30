4c5e387e4407081a8c132b7a68231f16271e4b09
Kristjan Lääne <xonproject@gmail.com>
2024-04-30 17:19:31 +0300

    Header start

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,9 +1,16 @@
+import Header from '@/components/Header';
+
 type Props = {
   children: React.ReactNode;
 };
 
-const layout = ({ children }: Props) => {
-  return <div className='felx felx-col min-h-screen'></div>;
+const Layout = ({ children }: Props) => {
+  return (
+    <div className='felx felx-col min-h-screen'>
+      <Header />
+      <div className='container mx-auto flex-1 py-10'>{children}</div>
+    </div>
+  );
 };
 
-export default layout;
+export default Layout;
