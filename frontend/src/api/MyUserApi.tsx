a7e42b86fb8155f9761340992b7952db3b99f57e
Kristjan Lääne <xonproject@gmail.com>
2024-05-01 14:18:53 +0300

    create user finish?

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,3 +1,5 @@
+import { useMutation } from 'react-query';
+
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 type CreateUserRequest = {
@@ -18,5 +20,18 @@ export const useCreateMyUser = () => {
     if (!response.ok) {
       throw new Error('Failed to create user');
     }
+  };
+  const {
+    mutateAsync: createUser,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useMutation(createMyUserRequest);
+
+  return {
+    createUser,
+    isLoading,
+    isError,
+    isSuccess,
   };
 };
